05e23ef43ca2c3bdc6312f686c7b000f0ff81f1a
ABINAYAkassy <abinayamurugesan9344@gmail.com>
2025-02-11 14:19:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            alert("Item added to cart!");
+        });
+    });
+});
 
